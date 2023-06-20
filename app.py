43d1dfd136fc99d15b0d8cbc42dfa50bf30c621e
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-06-20 13:26:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ import streamlit as st
 from transformers import pipeline
 
 def generate_response(input_text):
-    # Load the pre-trained Hugging Face model
     chatbot_model = pipeline("text-generation", model="microsoft/DialoGPT-medium")
-
-    # Generate a response from the chatbot model
     response = chatbot_model(input_text)[0]['generated_text']
     return response
 
@@ -13,13 +10,17 @@ def chat():
     st.title("Ka Chatbot")
     st.write("Welcome! I'm Ka, your friendly chatbot. How can I assist you today?")
 
+    input_counter = 0  # Counter to generate unique keys
+
     while True:
-        user_input = st.text_input("User:")
+        user_input = st.text_input(f"User-{input_counter}:")  # Append counter to the key
         if user_input.lower() in ["exit", "quit"]:
             st.write("Ka: Goodbye! Have a great day!")
             break
 
         response = generate_response(user_input)
         st.write("Ka:", response)
+
+        input_counter += 1  # Increment the counter
 
 chat()
