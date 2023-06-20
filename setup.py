f584ad8e4b9082e73efec29285d4b3ded4450d46
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-06-20 13:18:12 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ka-chatbot',
+    version='1.0',
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='Chatbot using Streamlit',
+    packages=find_packages(),
+    install_requires=[
+        'streamlit',
+        'transformers'
+    ],
+)
