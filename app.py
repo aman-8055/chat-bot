dece0566392ddb1382a173369579a366a3cf521f
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-06-20 13:54:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,37 @@
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
-# Load pre-trained model and tokenizer
-model_name = "distilbert-base-uncased-distilled-squad"  # Replace with your desired model
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model_name = "shivam001/dei-q-and-a"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+    return tokenizer, model
 
-# Streamlit app code
-def answer_question(context, question):
-    inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors="pt")
+def answer_question(question, tokenizer, model):
+    inputs = tokenizer.encode_plus(question, add_special_tokens=True, return_tensors="pt")
     input_ids = inputs["input_ids"].tolist()[0]
 
-    # Get the start and end logits for the answer
     with torch.no_grad():
-        start_logits, end_logits = model(**inputs).logits
+        outputs = model(**inputs)
 
-    start_index = torch.argmax(start_logits, dim=1).item()
-    end_index = torch.argmax(end_logits, dim=1).item()
-
-    # Convert the token IDs back to text
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[start_index:end_index+1]))
+    answer_start = torch.argmax(outputs.start_logits)
+    answer_end = torch.argmax(outputs.end_logits) + 1
+    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
 
     return answer
 
 def main():
     st.title("Question Answering Chatbot")
-    context = st.text_area("Context:", "Enter the context here...")
     question = st.text_input("Question:", "Enter your question here...")
 
     if st.button("Answer"):
-        if context and question:
-            answer = answer_question(context, question)
+        if question:
+            tokenizer, model = load_model()
+            answer = answer_question(question, tokenizer, model)
             st.write("Answer:", answer)
         else:
-            st.write("Please enter both context and question.")
+            st.write("Please enter a question.")
 
 if __name__ == "__main__":
     main()
